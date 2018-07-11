594c262f0e5779d897d593f0ed46d75a5a2ee3fa
Anurag Roy <anuragroy11@gmail.com>
2018-07-12 00:40:48 +0530

    SP-Arrays

---
+++ SudoPlacements/Arrays/06.Rotation.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    
+    cin >> t;
+    
+    while (t) {
+        
+        int d, n, temp;
+        vector<int> a;
+        cin >> n;
+        
+        for (int i = 0; i < n; i++) {
+            cin >> temp;
+            a.push_back(temp);
+        }
+        
+        cin >> d;
+        
+        while (d) {
+        
+            a.push_back(*a.begin());
+            a.erase(a.begin());
+            d--;
+        }
+    
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        
+        cout << endl;
+    
+        t--;
+    }
+    
+    return 0;
+}
