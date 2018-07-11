ff34b537badf1c6a75d08e938e883bf56a271c5c
Anurag Roy <anuragroy11@gmail.com>
2018-07-12 00:31:22 +0530

    x

---
+++ SudoPlacements/Arrays/x.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    
+    cin >> t;
+    
+    while (t) {
+        
+        int n, temp;
+        vector<int> a;
+        cin >> n;
+        
+        for (int i = 0; i < n; i++) {
+            cin >> temp;
+            a.push_back(temp);
+        }
+        
+        t--;
+    }
+    
+    return 0;
+}
