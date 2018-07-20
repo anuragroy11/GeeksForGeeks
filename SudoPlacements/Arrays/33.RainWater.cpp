193039024cd0e3da910b27f6269f7d28b1c2092e
Anurag Roy <anuragroy11@gmail.com>
2018-07-20 16:51:03 +0530

    SP-Arrays

--- SudoPlacements/Arrays/33.RainWater.cpp
+++ SudoPlacements/Arrays/33.RainWater.cpp
@@ -1,18 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int min(int a, int b) {
-    if (a < b)
-        return a;
-    else return b;
-}
-
-int max(int a, int b) {
-    if (a > b)
-        return a;
-    else return b;
-}
-
 int main() {
     
     int t;
