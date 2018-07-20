d5ba5dbd285c1557c349ca435eac80ccc26f7449
Anurag Roy <anuragroy11@gmail.com>
2018-07-20 15:40:06 +0530

    SP-Arrays

---
+++ SudoPlacements/Arrays/33.RainWater.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int min(int a, int b) {
+    if (a < b)
+        return a;
+    else return b;
+}
+
+int max(int a, int b) {
+    if (a > b)
+        return a;
+    else return b;
+}
+
+int main() {
+    
+    int t;
+    
+    cin >> t;
+    
+    while (t) {
+        
+        int n, temp, sum = 0;
+        vector<int> a;
+        cin >> n;
+        
+        for (int i = 0; i < n; i++) {
+            cin >> temp;
+            a.push_back(temp);
+        }
+        
+        vector<int> dpleft(n), dpright(n);
+        
+        for (int i = 0; i < n; i++) {
+            dpleft[i] = *max_element(a.begin(), a.begin() + i);
+            dpright[i] = *max_element(a.begin() + i, a.end());
+        }
+        
+        for (int i = 1; i < n - 1; i++) {
+            int x = max(min(dpleft[i], dpright[i]) - a[i], 0);
+            sum = sum + x;
+        }
+        
+        cout << sum << endl;
+        
+        t--;
+    }
+    
+    return 0;
+}
