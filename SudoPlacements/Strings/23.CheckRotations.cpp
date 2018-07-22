c650b62859fae1b96cb1310fb5f91c498d35a93e
Anurag Roy <anuragroy11@gmail.com>
2018-07-23 01:09:04 +0530

    SP-Strings

---
+++ SudoPlacements/Strings/23.CheckRotations.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    
+    while (t--){
+        
+        string a, b;
+        cin >> a >> b;
+        
+        if(a.length() == b.length() && (a+a).find(b) != string::npos) {
+            cout << 1 << endl;
+        }
+        else {
+            cout << 0 << endl;
+        }
+    }
+    
+    return 0;
+}
