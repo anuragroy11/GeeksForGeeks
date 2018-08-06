c96997aa4c2cf29e98a9a61748282aa10e89e466
Anurag Roy <anuragroy11@gmail.com>
2018-08-06 21:05:49 +0530

    SP-Graph

---
+++ SudoPlacements/Graph/03.SumOfDependencies.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int m, n;
+        cin >> m >> n;
+        for (int i = 0; i < 2 * n; i++)
+            cin >> m;
+            
+        cout << n << endl;
+    }
+    
+    return 0;
+}
